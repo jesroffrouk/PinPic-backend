450b929e9a580ba780f5595ab1c0c11cce62a89a
jesroffrouk <jesroffrouk@gmail.com>
2026-01-30 06:33:58 +0000

    added new schema and fixed so many bugs

--- models/placesModels.js
+++ models/placesModels.js
@@ -4,94 +4,103 @@ import { createLoggerFor } from '../helpers/loggers/loggers.js';
 const logger = createLoggerFor(import.meta.url, 'db service');
 
 const placesModels = {
-    setImages: async (captions, imgurl, longitude, latitude, userid) => {
+    getIdFromPublicId: async (table,public_id) => {
+        logger.info('getting id');
+        return await db.query(
+            `Select id 
+             From ${table}
+             WHERE public_id = $1`,
+            [public_id]
+        );
+    },
+    setImages: async (captions, imgurl, longitude, latitude, userId) => {
         logger.info('adding Image to db');
         return await db.query(
-            `INSERT INTO places (captions,imgurl,location,userid) VALUES ($1,$2, ST_GeogFromText('POINT('|| $3 || ' '|| $4 || ' )'), $5 ) RETURNING *`,
-            [captions, imgurl, longitude, latitude, userid]
+            `INSERT INTO posts (captions,imgurl,location,user_id) VALUES ($1,$2, ST_GeogFromText('POINT('|| $3 || ' '|| $4 || ' )'), $5 ) RETURNING *`,
+            [captions, imgurl, longitude, latitude, userId]
         );
     },
 
-    getImages: async (longitude, latitude, userid) => {
-        console.log(longitude, latitude, userid);
+    getImages: async (longitude, latitude, userId) => {
+        console.log(longitude, latitude, userId);
         logger.info('getting images from db');
         return await db.query(
             `SELECT 
-      places.id,
-      places.location,
-      places.captions,
-      places.imgurl,
+      posts.public_id as id,
+      posts.location,
+      posts.captions,
+      posts.imgurl,
       users.username AS author_name,
-      COUNT(DISTINCT CASE WHEN upvotes.react_type = 'like' THEN upvotes.id END) AS upvotes_count,
+      COUNT(DISTINCT CASE WHEN votes.react_type = 'upvoted' THEN votes.id END) AS upvotes_count,
           CASE 
               WHEN EXISTS (
                   SELECT 1 
-                  FROM upvotes u2 
-                  WHERE u2.imgid = places.id 
-                    AND u2.userid = $3
-                    AND u2.react_type = 'like'
+                  FROM votes u2 
+                  WHERE u2.img_id = votes.id 
+                    AND u2.user_id = $3
+                    AND u2.react_type = 'upvoted'
               ) 
               THEN true ELSE false
           END AS upvoted
-      FROM places 
-      JOIN users ON places.userid = users.id 
-      LEFT JOIN upvotes ON places.id = upvotes.imgid 
+      FROM posts 
+      JOIN users ON posts.user_id = users.id 
+      LEFT JOIN votes ON posts.id = votes.img_id 
       WHERE ST_DWithin(
           location, 
           ST_GeogFromText($1), 
           $2
       )
-      GROUP BY places.id, users.username
+      GROUP BY posts.id, users.username , votes.id
       ORDER BY upvotes_count;
             `,
-            [`SRID=4326;POINT(${longitude} ${latitude})`, 1000, userid]
+            [`SRID=4326;POINT(${longitude} ${latitude})`, 1000, userId]
         );
     },
-    doesUpoteExist: async (userid, imgid) => {
+    doesUpoteExist: async (userId, imgId) => {
         logger.info('getting upvote..');
         return await db.query(
             `SELECT id , react_type
-            FROM upvotes
-            WHERE userid = $1
-            AND imgid = $2
+            FROM votes
+            WHERE user_id = $1
+            AND img_id = $2
         `,
-            [userid, imgid]
+            [userId, imgId]
         );
     },
-    setNewUpvote: async (userid, imgid, react_type) => {
+    setNewUpvote: async (userId, imgId, react_type) => {
         logger.info('creating new upvote');
         return await db.query(
             `
-            INSERT INTO upvotes (userid, imgid, react_type)
+            INSERT INTO votes (user_id, img_id, react_type)
             VALUES ($1,$2,$3);
             `,
-            [userid, imgid, react_type]
+            [userId, imgId, react_type]
         );
     },
-    setExistingUpvote: async (id, react_type) => {
+    setExistingUpvote: async (id, reactType) => {
         logger.info('setting exisitng upvote to this image');
         return await db.query(
             `
-            UPDATE upvotes
+            UPDATE votes
             SET react_type = $1
             WHERE id = $2;
             `,
-            [react_type, id]
+            [reactType, id]
         );
     },
-    getUserFromimgid: async (imgid) => {
+    getUserFromimgid: async (imgId) => {
         logger.info('getting userid from imgid');
         return await db.query(
             `
             SELECT users.id FROM users JOIN
-            places ON users.id = places.userid 
-            WHERE places.id = $1
+            posts ON users.id = votes.user_id 
+            WHERE posts.id = $1
             `,
-            [imgid]
+            [imgId]
         );
     },
-    createNotification: async ({ type, recipient_id, actor_id, metaData }) => {
-        console.log(type,recipient_id,actor_id,metaData)
+    createNotification: async (type, recipientId, actorId, metaData) => {
+        console.log(type,recipientId,actorId,metaData)
         logger.info('createing new notification...');
         return await db.query(
             `
@@ -100,10 +109,10 @@ const placesModels = {
       VALUES ($1,$2,$3,$4)
       RETURNING id
       `,
-            [type, recipient_id, actor_id, metaData]
+            [type, recipientId, actorId, metaData]
         );
     },
-    getNotification: async (user_id) => {
+    getNotification: async (userId) => {
         logger.info('getting all notificaton for user');
         return await db.query(
             `
@@ -111,18 +120,19 @@ const placesModels = {
       WHERE recipient_id = $1
       ORDER BY created_at DESC
       `,
-            [user_id]
+            [userId]
         );
     },
-    getNotificationToSend: async (recipient_id) => {
+    getNotificationToSend: async (recipientId) => {
         logger.info('getting all notificaton for user to send');
+        console.log(recipientId)
         return await db.query(
             `
       SELECT id,type,recipient_id,created_at FROM notifications 
       WHERE recipient_id = $1 and is_sent = false
       ORDER BY created_at ASC
       `,
-            [recipient_id]
+            [recipientId]
         );
     },
     setNotificationtoSent: async(notificationId) => {
--- services/placesServices.js
+++ services/placesServices.js
@@ -6,7 +6,7 @@ import { sendNotification } from '../helpers/socket/notification.js';
 const logger = createLoggerFor(import.meta.url, 'places Services');
 
 const placesServices = {
-  uploadImage: async (userid, captions, longitude, latitude, fileBase64) => {
+  uploadImage: async (userPublicId, captions, longitude, latitude, fileBase64) => {
     //image upload to cloudnary
     logger.info('uploading images to cloudinary');
     const cloudResponse = await cloudinary.uploader.upload(fileBase64, {
@@ -15,57 +15,59 @@ const placesServices = {
     logger.info('uploaded to cloudinary');
     logger.info('saving it to db');
     const imgurl = cloudResponse.secure_url;
-    const result = await placesModels.setImages(
+    // get id from public_id
+    const {id: userId} = (await placesModels.getIdFromPublicId('users',userPublicId))?.rows[0]
+    await placesModels.setImages(
       captions,
       imgurl,
       longitude,
       latitude,
-      userid
+      userId
     ).rows;
+    // reduce it and only send public_id not id
     logger.info('upload Image successfull');
-    return { result };
+    return { success: true,message: 'upload image successfull' };
   },
-  getImageByLocation: async (longitude, latitude, userid) => {
+  getImageByLocation: async (longitude, latitude, userPublicId) => {
     logger.info('getting images from database');
-    const result = await placesModels.getImages(longitude, latitude, userid);
+    // userid must be public_Id so request and change it to id before use
+    const {id:userId} = (await placesModels.getIdFromPublicId('users',userPublicId))?.rows[0]
+    const result = await placesModels.getImages(longitude, latitude, userId);
     const data = result.rows;
     logger.info('sorting the images..');
     logger.info('image retrived successful');
     return { data };
   },
-  upVoteImage: async (userid, imgid, react_type) => {
-    const doesUpvoteExist = await placesModels.doesUpoteExist(userid, imgid); // expecting object
+  upVoteImage: async (userPublicId, imgPublicId, reactType) => {
+    // get id from publicid
+    // change imgid to postsId instead --for future
+    const {id: userId} = (await placesModels.getIdFromPublicId('users',userPublicId))?.rows[0]
+    const {id: imgId} = (await placesModels.getIdFromPublicId('posts',imgPublicId))?.rows[0]
+    const doesUpvoteExist = await placesModels.doesUpoteExist(userId, imgId); // expecting object
     if (doesUpvoteExist.rowCount > 0) {
       // update it with react type so match react type if it matches update otherwise don't also check it's is_active
       // matching if current react type is same as before,
       // also need to check if imgid is valid one in db, and can current user authorize to like it or not
       await placesModels.setExistingUpvote(
         doesUpvoteExist.rows[0].id,
-        react_type
+        reactType
       );
       logger.info('updated the upvote');
       // notification creation is not working for this section
       return { message: 'updated the upvote reaction', success: true };
     }
     // create a new table for this upvote
-    await placesModels.setNewUpvote(userid, imgid, react_type);
+    await placesModels.setNewUpvote(userId,imgId,reactType);
     // get author from imgid
-    const recipient_id = (await placesModels.getUserFromimgid(imgid)).rows[0].id;
+    const recipientId = (await placesModels.getUserFromimgid(imgId))?.rows[0].id;
     // create notification
-    const notification = {
-      type: 'like',
-      recipient_id: recipient_id,
-      actor_id: userid,
-      metaData: {
-        post: imgid,
-      },
-    };
-    const notificationId = await placesModels.createNotification(notification);
+    // using userId as actorId for this call
+    const notificationId = await placesModels.createNotification('upvoted',recipientId,userId,{post: imgId});
     // send online notification
     const notData = {
-      recipient_id,
+      recipientId,
       notificationId,
-      type: notification.type
+      type: 'upvoted'
     }
     await sendNotification(notData)
     logger.info('successfully created new upvote for user');
