9179e03b591de935ef7b0b8fc233a6775120215c
jesroffrouk <jesroffrouk@gmail.com>
2026-09-06 10:18:41 +0530

    fixing mismatch cloudinary url

--- services/authServices.js
+++ services/authServices.js
@@ -179,7 +179,7 @@ function createAuthServices({
       const unsigneddUrlUser = await userRepository.getUserProfile(userId);
 
       let user;
-      if (unsigneddUrlUser) {
+      if (unsigneddUrlUser.profile_url) {
         user = {
           ...unsigneddUrlUser,
           profile_url: generateSignedUrl(unsigneddUrlUser.profile_url),
